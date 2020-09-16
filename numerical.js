693bd43c9ef70bf3d2628dba31b24c6531d5e87d
Rochelle Chan <rochelle.r.chan@gmail.com>
2020-09-16 10:15:08 -0700

    initial commit

---
+++ numerical.js
@@ -0,0 +1,5 @@
+// Question #2
+// Write a function that checks if an inputted value is a numerical range "100-200". 
+// Inputted values can be an integer or a string in the previously stated format. 
+// The return should be a true/false (bool) value. Ranges should also allow floats (e.g. "100.00"). 
+// The range should also be listed as min/max order. Valid values: 100-200, 100.0-200.1. Invalid Values: 100, 200-100.
---
+++ palindrome.js
@@ -0,0 +1,32 @@
+// Question #1
+// Write a function that checks if an inputted value is a palindrome. 
+// The function should return true/false (bool). 
+// You can assume that all input will be a string type and lower case.
+
+
+// First Solution
+// 1. Simple solution using .split(). This is needed to split the string into an array
+// 2. After we can the javascript method .reverse() to reverse the order of the array
+// 3. .join() simply concatenate the array back to a string
+// 4. Use strict type to compare the input passed to the reverse string variable
+
+checkIfPalindrome = (input) => {
+    // regex expression to find spaces
+    const regexExp = /\s/g; 
+    // using regex expression to replace with empty string
+    const original = input.replace(regexExp, '');
+    const reverseInput = original.split('').reverse().join('');
+    return reverseInput === original;
+};
+
+// Test cases to run
+const firstInput = 'eye';
+const secondInput = 'race car';
+const thirdInput = 'never odd or even';
+const fourthInput = 'test';
+
+// Calling the function and passing each variable
+console.log('First Test Case:', checkIfPalindrome(firstInput));
+console.log('Second Test Case:', checkIfPalindrome(secondInput));
+console.log('Third Test Case:', checkIfPalindrome(thirdInput));
+console.log('Fourth Test Case:', checkIfPalindrome(fourthInput));
